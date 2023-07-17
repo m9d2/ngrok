24dbd829d9c98a9baccab1131d577ba9632f8d90
gaoy <158612783@qq.com>
2023-07-18 00:20:42 +0800

    update

---
+++ dependency/termbox-go/go.mod
@@ -0,0 +1,7 @@
+module github.com/badge/termbox-go
+
+go 1.20
+
+require github.com/mattn/go-runewidth v0.0.14
+
+require github.com/rivo/uniseg v0.2.0 // indirect
---
+++ dependency/termbox-go/terminfo_builtin_test.go
@@ -0,0 +1,16 @@
+//go:build !windows
+// +build !windows
+
+package termbox
+
+import "testing"
+
+func TestTerminfoTerms(t *testing.T) {
+	for _, term := range terms {
+		t.Run(term.name, func(t *testing.T) {
+			if len(term.funcs) != t_max_funcs {
+				t.Errorf("want %d got %d terminfo entries", t_max_funcs, len(term.funcs))
+			}
+		})
+	}
+}
